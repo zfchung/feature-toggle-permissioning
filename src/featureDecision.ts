e4eaf0104dc3c556cb7fabbfb39ceba180410f70
ZF Chung (Zack) <zfchung1@gmail.com>
2021-09-20 17:11:31 +0800

    editted config format

--- src/featureDecision.ts
+++ src/featureDecision.ts
@@ -1,6 +1,7 @@
 export function createFeatureDecisions(features: any){
     return {
         includeWeightInBabyDetails(){
+            // check for user permissions
             return features.isEnabled("include-baby-weight");
         }
     }
