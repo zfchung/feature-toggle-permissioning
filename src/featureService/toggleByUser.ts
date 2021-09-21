2e2786f499da3c9525e2a15c76463690156b7074
ZF Chung (Zack) <zfchung1@gmail.com>
2021-09-21 12:07:14 +0800

    created toggleByUser.ts, able to get featureList by user

---
+++ src/featureService/toggleByUser.ts
@@ -0,0 +1,18 @@
+const featureConfig = require("./featureConfig.json");
+const USER_ID_SESSION = "randomUser"; // userid of person logged in
+
+function getUserAllowedFeatures(userID: string): string[] {
+    return featureConfig.userList[userID];
+}
+
+const featureOpenForUser = getUserAllowedFeatures(USER_ID_SESSION);
+const featureList: object = featureConfig.featureList;
+
+const filtered = Object.fromEntries(
+    Object.entries(featureList).filter(
+        ([key, val])=>featureOpenForUser.includes(key)
+    )
+);
+
+
+console.log("filtered",filtered);
