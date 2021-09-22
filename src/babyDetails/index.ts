3b841da93d40ab9af72bef93b61b541401c973c3
ZF Chung (Zack) <zfchung1@gmail.com>
2021-09-22 15:06:39 +0800

    babyDetails passess all test

--- src/babyDetails/index.ts
+++ src/babyDetails/index.ts
@@ -1,27 +1,27 @@
-// import {createToggleRouter} from "../featureService/toggleRouter";
-// import {createFeatureService} from "../featureService/featureService";
-// import {getFeaturesByUserPermission} from "../featureService/toggleByParameter";
-// const featureConfig = getFeaturesByUserPermission();
-// const features = createToggleRouter(featureConfig);
-// const featureService = createFeatureService(features);
-//
-// export function inputBabyDetailsOf(babyName: string) {
-//     if (featureService.includeWeightInBabyDetails()){
-//         return newInputBabyDetailsOf(babyName);
-//     } else {
-//         return oldInputBabyDetailsOf(babyName);
-//     }
-// }
-//
-// function newInputBabyDetailsOf(babyName: any){
-//     const gender = babyName.gender;
-//     const weight = babyName.weight;
-//     const babyDetails = [gender, weight];
-//     return babyDetails;
-// }
-//
-// function oldInputBabyDetailsOf(babyName: any){
-//     const gender = babyName.gender;
-//     const babyDetails = [gender];
-//     return babyDetails;
-// }
+import {createToggleRouter} from "../featureService/toggleRouter";
+import {createFeatureService} from "../featureService/featureService";
+import {getFeatureList} from "../featureService/toggleByParameter";
+const featureConfig = getFeatureList();
+const features = createToggleRouter(featureConfig);
+const featureService = createFeatureService(features);
+
+export function inputBabyDetailsOf(babyName: string) {
+    if (featureService.includeWeightInBabyDetails()){
+        return newInputBabyDetailsOf(babyName);
+    } else {
+        return oldInputBabyDetailsOf(babyName);
+    }
+}
+
+function newInputBabyDetailsOf(babyName: any){
+    const gender = babyName.gender;
+    const weight = babyName.weight;
+    const babyDetails = [gender, weight];
+    return babyDetails;
+}
+
+function oldInputBabyDetailsOf(babyName: any){
+    const gender = babyName.gender;
+    const babyDetails = [gender];
+    return babyDetails;
+}
